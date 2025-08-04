e80eded3b1df04c1e4d4df7a132d745587d2d59f
PhatJack <tienphat.ng693@gmail.com>
2025-08-04 10:44:56 +0700

    feat: add auth page

---
+++ src/app/(auth)/_components/ForgotPWPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ForgotPWPage = () => {
+	return (
+		<div>ForgotPWPage</div>
+	)
+}
+
+export default ForgotPWPage
---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+import ForgotPWPage from "../_components/ForgotPWPage";
+
+export default function ForgotPassword() {
+  return <ForgotPWPage />;
+}
