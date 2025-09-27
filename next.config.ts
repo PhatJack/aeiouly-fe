f110a845375862b40f6fe2325a626f9664abb5ed
PhatJack <tienphat.ng693@gmail.com>
2025-09-27 11:38:01 +0700

    refactor: update image remote patterns in next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-	devIndicators: false,
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aeiouly.s3.ap-southeast-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
